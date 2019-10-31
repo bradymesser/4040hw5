bde2996b0970922d0483f81430a22099d9d4d0d2
wmesser <wmesser@g.clemson.edu>
2019-10-31 11:47:02 -0400

    Setup

--- makefile
+++ makefile
@@ -3,10 +3,12 @@ FILTER = Filter
 IMAGE = Image
 RGBTOHSV = rgbToHsv
 GLOBALS = Globals
+OBJECTS = ${PROJECT}.o matrix.o ${FILTER}.o ${IMAGE}.o ${RGBTOHSV}.o ${GLOBALS}.o
 
 default:
 	g++ -c -g *.cpp
-	g++ -g -o ${PROJECT} ${PROJECT}.o ${FILTER}.o ${IMAGE}.o ${RGBTOHSV}.o ${GLOBALS}.o -L /usr/lib64/ -lglut -lGL -lGLU -lOpenImageIO -lm
+	g++ -g -o ${PROJECT} ${OBJECTS} -L /usr/lib64/ -lglut -lGL -lGLU -lOpenImageIO -lm
+	
 clean:
 	rm -f core.* *.o *~ ${PROJECT}
 
@@ -19,3 +21,30 @@ square:
 	-make clean
 	-make default
 	./convolve filters/pulse.filt images/square.png
+
+
+# CC      = g++
+# C       = cpp
+#
+# CFLAGS  = -g
+#
+# ifeq ("$(shell uname)", "Darwin")
+#   LDFLAGS     = -framework Foundation -framework GLUT -framework OpenGL -lOpenImageIO -lm
+# else
+#   ifeq ("$(shell uname)", "Linux")
+#     LDFLAGS   = -L /usr/lib64/ -lglut -lGL -lGLU -lOpenImageIO -lm
+#   endif
+# endif
+#
+# PROJECT		= warper
+#
+# OBJECTS = ${PROJECT}.o matrix.o
+#
+# ${PROJECT}:	${PROJECT}.o matrix.o
+# 	${CC} ${CFLAGS} ${LFLAGS} -o ${PROJECT} ${OBJECTS} ${LDFLAGS}
+#
+# %.o: %.cpp
+# 	${CC} -c ${CFLAGS} *.${C}
+#
+# clean:
+# 	rm -f core.* *.o *~ ${PROJECT}
--- warper.cpp
+++ warper.cpp
@@ -1,5 +1,118 @@
+/*
+ * Program sample to warp an image using matrix-based warps
+ *
+ * Command line parameters are as follows:
+ *
+ * warper infile.png [outfile.png]
+ *
+ * Author: Ioannis Karamouzas, 10/20/19
+ */
+
+#include "matrix.h"
 #include "Helper.h"
 
-int main() {
-  return 0;
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::string;
+
+/*
+Multiply M by a rotation matrix of angle theta
+*/
+
+void Rotate(Matrix3D &M, float theta) {
+	int row, col;
+	Matrix3D R;
+	double rad, c, s;
+	rad = PI * theta / 180.0;
+	c = cos(rad);
+	s = sin(rad);
+	R[0][0] = c;
+	R[0][1] = s;
+	R[1][0] = -s;
+	R[1][1] = c;
+
+	M = R * M;
+
+}
+
+
+/*
+Build a transformation matrix from input text
+*/
+void read_input(Matrix3D &M) {
+	string cmd;
+
+	/* prompt for user input */
+	do
+	{
+		cout << "> ";
+		cin >> cmd;
+		if (cmd.length() != 1){
+			cout << "invalid command, enter r, s, t, h, f, p, d\n";
+		}
+		else {
+			switch (cmd[0]) {
+				case 'r':		/* Rotation, accept angle in degrees */
+					float theta;
+					cin >> theta;
+					if (cin) {
+						cout << "calling rotate\n";
+						Rotate(M, theta);
+					}
+					else {
+						cerr << "invalid rotation angle\n";
+						cin.clear();
+					}
+					break;
+				case 's':		/* scale, accept scale factors */
+					break;
+				case 't':		/* Translation, accept translations */
+					break;
+				case 'h':		/* Shear, accept shear factors */
+					break;
+				case 'f':		/* Flip, accept flip factors */
+					break;
+				case 'p':		/* Perspective, accept perspective factors */
+					break;
+				case 'd':		/* Done, that's all for now */
+					break;
+				default:
+					cout << "invalid command, enter r, s, t, h, f, p, d\n";
+			}
+		}
+	} while (cmd.compare("d")!=0);
+
+}
+
+/*
+   Main program to read an image file, then ask the user
+   for transform information, transform the image and display
+   it using the appropriate warp.  Optionally save the transformed
+   images in  files.
+*/
+int main(int argc, char *argv[]){
+
+	// initialize transformation matrix to identity
+	Matrix3D M;
+
+   //your code to read in the input image
+
+
+   //build the transformation matrix based on user input
+   read_input(M);
+
+
+	// printout the final matrix
+    cout << "Accumulated Matrix: " << endl;
+    M.print();
+
+   // your code to perform inverse mapping (4 steps)
+
+   //your code to display the warped image
+
+   return 0;
 }
